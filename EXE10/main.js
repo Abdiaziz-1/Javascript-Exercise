e6df7142e0839f9f338c453b474d10378eddf305
Abdiaziz-1 <abdimoham100@gmail.com>
2025-12-21 21:44:41 +0300

    Exercise 10

---
+++ EXE10/main.js
@@ -0,0 +1,17 @@
+let libarary = [
+    {
+        title : "Atomic habits",
+        author : " James Clear",
+        year : 2018
+    },
+
+    {
+        title : "think and grow rich",
+        author : "Napoleon Hill",
+        year : 1937
+    }
+];
+
+console.log(libarary[0].title);
+
+console.log(libarary[1].author)
