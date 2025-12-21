ec0308371bf0fea328254053ef29de8be9d1d8cb
Abdiaziz-1 <abdimoham100@gmail.com>
2025-12-21 04:09:09 +0300

    Exercise 5

---
+++ EXE5/main.js
@@ -0,0 +1,9 @@
+const add = (product, price) => {
+    return product + price
+}
+
+let total1 = add(20,30)
+console.log("This is the " + total1)
+
+let total2 = add(50,80)
+console.log("Total2 is " + total2)
