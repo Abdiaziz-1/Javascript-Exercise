611087176d546d6d406337ef75b55daab2f997e2
Abdiaziz-1 <abdimoham100@gmail.com>
2025-12-24 03:18:20 +0300

    exercise 17

---
+++ Exercise17/main.js
@@ -0,0 +1,15 @@
+
+
+let temp = 8;
+
+if(temp < 0) {
+    console.log("very cold")
+} else if (temp >= 0 && temp < 15) {
+    console.log("cold")
+} else if (temp >= 15 && temp <= 25) {
+    console.log("warm")
+} else {
+    console.log("Hot")
+}
+
+
