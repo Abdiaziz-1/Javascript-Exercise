249356e1b51e308853e97b0c825cd9c407548e1d
Abdiaziz-1 <abdimoham100@gmail.com>
2026-01-01 11:09:56 +0300

    exercise 32

---
+++ Exercise32/main.js
@@ -0,0 +1,12 @@
+
+const heading = document.querySelector("h1")
+
+console.log(heading)
+
+const paragraphs = document.querySelectorAll(".par")
+
+console.log(paragraphs)
+
+const uniquePar = document.querySelector("#p1")
+
+console.log(uniquePar)
